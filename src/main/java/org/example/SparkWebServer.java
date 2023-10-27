e00d7a14ae631fa4baeb815f888d8bd72d38eea1
stefania5964 <tefila123@hotmail.com>
2023-10-26 20:01:53 -0500

    c

--- src/main/java/org/example/SparkWebServer.java
+++ src/main/java/org/example/SparkWebServer.java
@@ -7,25 +7,34 @@ import java.util.List;
 import static spark.Spark.*;
 public class SparkWebServer {
     private static List<Integer> miLista = new ArrayList<Integer>();
-    private int variable = 0;
-    public static void main(String... args){
 
+    public static void main(String... args) {
         port(getPort());
-        get("hello", (req,res) -> "Hello Docker!");
 
-        get("lucasseq", (req,res) -> {
+        // Endpoint para cargar la página con el formulario
+        get("/lucasseq", (req, res) -> {
+            res.type("text/html");
+            return "<html><body>" +
+                    "<h1>Secuencia de Lucas</h1>" +
+                    "<form method='post' action='/lucasseq'>" +
+                    "Ingrese un valor: <input type='text' name='value'><br>" +
+                    "<input type='submit' value='Calcular'>" +
+                    "</form>" +
+                    "</body></html>";
+        });
+
+        // Endpoint POST para procesar el formulario y devolver una respuesta JSON
+        post("/lucasseq", (req, res) -> {
+            res.type("application/json");
             int value = Integer.parseInt(req.queryParams("value"));
             List<Integer> x = getLucas(value);
             String operation = "Secuencia de Lucas";
 
-            return "" +
-                    "{\n" +
+            return "{\n" +
                     "\t\"operation\": \"" + operation + "\",\n" +
-                    "\t\"input\": " + value + ",\n"+
-                    "\t\"output\": \"" + x + "\"\n" +
+                    "\t\"input\": " + value + ",\n" +
+                    "\t\"output\": " + x + "\n" +
                     "}";
-
-
         });
     }
 
@@ -33,29 +42,22 @@ public class SparkWebServer {
         if (System.getenv("PORT") != null) {
             return Integer.parseInt(System.getenv("PORT"));
         }
-        return 5000;
+        return 4567;
     }
-    private static int getx(int x) {
-        return x;
-    }
-    private static List<Integer> getLucas(int x){
-        getx(x);
+
+    private static List<Integer> getLucas(int x) {
         miLista.clear();
-        int a=2;
-        int b=1;
+        int a = 2;
+        int b = 1;
         miLista.add(a);
         miLista.add(b);
-        for(int i = 2; i <= x; i = i + 1) {
+        for (int i = 2; i <= x; i++) {
             int c = miLista.get(i - 1) + miLista.get(i - 2);
             miLista.add(c);
         }
         return miLista;
-
-
-
-
     }
-
-
-
 }
+
+
+
